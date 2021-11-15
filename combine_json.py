e6c2100b33c73ca650ef4e0d7e809ca511d759fe
joseph86 <josephparish@gmail.com>
2021-11-15 17:05:17 -0500

    Update combine_json.py
    Fixed bugs from opening files for binary read and write. Added output  of a valid json file.

--- combine_json.py
+++ combine_json.py
@@ -1,13 +1,20 @@
 import json
 import glob
 
-# set up list of combined json file data and input glob
-combined_json = []
+# set up list for combined json file data and input glob
+combined_list = []
 input_files = glob.glob("*.json")
 
+# open all json files in the glob and add them to the combined list
 for f in input_files:
-  with open(f, "rb") as current_file:
-    combined_json.append(json.load(current_file))
+  with open(f, "r") as current_file:
+    combined_list.append(json.load(current_file))
     
 #now print the output just to test
-print(combined_json)
+#for i in combined_list:
+#    print(i)
+#    print('\n')
+
+# write combined json file
+with open("combined_json_file.json", "w") as fileout:
+    json.dump(combined_list, fileout)
